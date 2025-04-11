3c2bec46928302556ba3eb131a098b8c5e364eb9
sita1013 <seluvaiasariahita@gmail.com>
2025-04-11 09:13:32 +0100

    adjusting countries_list to countries_details

--- countriesApp/urls.py
+++ countriesApp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.country_list, name='country_list'),
-    path('countries/<str:country_code>/', views.country_detail, name='country_detail'),
+    path('', views.country_detail, name='country_list'),
+    #path('countries/<str:country_code>/', views.country_detail, name='country_detail'),
 ]
